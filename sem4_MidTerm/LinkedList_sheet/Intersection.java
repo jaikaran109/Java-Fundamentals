788087c48bf71d36f95a7f9392ba048f2e9a6bc9
jaikaran109 <jaik05581@gmail.com>
2026-03-13 13:37:34 +0530

    Update Intersection.java

--- sem4_MidTerm/LinkedList_sheet/Intersection.java
+++ sem4_MidTerm/LinkedList_sheet/Intersection.java
@@ -5,17 +5,47 @@
 //Code Stub
 
 
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        ListNode x = headA;
-        while(x != null) {
-            ListNode y = headB;
-            while(y != null){
-                if(x == y) return x;
-                y = y.next;
-            }
-            x = x.next;
+        ListNode A = headA;
+        ListNode B = headB;
+        while(A != B){
+            if(A == null) A = headB;
+            else A = A.next;
+            if(B == null) B = headA;
+            else B = B.next;
         }
-        return null;
+        return A;
     }
 }
+
+
+
+
+// public class Solution {
+//     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+//         ListNode x = headA;
+//         while(x != null) {
+//             ListNode y = headB;
+//             while(y != null){
+//                 if(x == y) return x;
+//                 y = y.next;
+//             }
+//             x = x.next;
+//         }
+//         return null;
+//     }
+// }
