6d1b0f3aa902892b1efdcd5defde119524fcba7f
jaikaran109 <jaik05581@gmail.com>
2026-04-22 20:17:03 +0530

    Create countVowelCons.java

---
+++ DSA_LAB/String/countVowelCons.java
@@ -0,0 +1,27 @@
+package String;
+import java.util.*;
+
+public class countVowelCons {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String str = input.nextLine().toLowerCase();
+
+        int vowels = 0;
+        int consonants = 0;
+
+        for(int i = 0; i < str.length(); i++){
+            char ch = str.charAt(i);
+
+            if(ch >= 'a' && ch <= 'z'){
+                if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
+                    vowels++;
+                } else {
+                    consonants++;
+                }
+            }
+        }
+
+        System.out.println("Vowels: " + vowels);
+        System.out.println("Consonants: " + consonants);
+    }
+}
