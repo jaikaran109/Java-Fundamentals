fa420cae126c7784eed38ff980e9e1679fbd664e
jaikaran109 <jaik05581@gmail.com>
2026-04-23 16:00:48 +0530

    Create reverseArray.java

---
+++ DSA_LAB/Array/reverseArray.java
@@ -0,0 +1,28 @@
+package Array;
+import java.util.*;
+public class reverseArray {
+    public static int[] reverse(int[] arr) {
+        int left = 0;
+        int right = arr.length - 1;
+        while (left < right) {
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+        return arr;
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = input.nextInt();
+        }
+        reverse(arr);
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
