4b9227c44e3212bfaf85c76ff5b2ff6a05d9e328
jaikaran109 <jaik05581@gmail.com>
2026-04-23 15:29:15 +0530

    Create previousSmallerElement.java

---
+++ DSA_LAB/Stack/previousSmallerElement.java
@@ -0,0 +1,35 @@
+package Stack;
+import java.util.*;
+public class previousSmallerElement {
+    public static int[] find(int[] arr) {
+        int n = arr.length;
+        int[] result = new int[n];
+        Stack<Integer> st = new Stack<>();
+        for (int i = 0; i < n; i++) {
+            while (!st.isEmpty() && st.peek() >= arr[i]) {
+                st.pop();
+            }
+            if (st.isEmpty()) {
+                result[i] = -1;
+            } else {
+                result[i] = st.peek();
+            }
+            st.push(arr[i]);
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = input.nextInt();
+        }
+        int[] result = find(arr);
+        for (int i = 0; i < n; i++) {
+            System.out.print(result[i] + " ");
+        }
+    }
+}
+
+// Time Complexity : O(n)
