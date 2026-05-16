d1284f133a44afa48e7f9702f6559e8941be9a76
jaikaran109 <jaik05581@gmail.com>
2026-05-17 02:38:33 +0530

    Create RemoveDuplicatesFromSortedArray.java

---
+++ sem4_EndTerm/Array/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int n = nums.length;
+        int l = 0;
+        int r = 1;
+        while(r < n){
+            if(nums[l] == nums[r]) r++;
+            else nums[++l] = nums[r];
+        }
+        return l + 1;
+    }
+}
